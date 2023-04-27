3b074e260be95f2e445e8507ce827899d5a3b20c
ejangelico <ejangelico@uchicago.edu>
2023-04-27 15:30:34 -0700

    I've merged the two types of RGA readers into one data structure with a different parsing function. I've also added the ability to export to a file format that the NIST MS Search tool can interpret. The Plotter file is not quite a tutorial here and is messy.

--- RGAReader.py
+++ RGAReader.py
@@ -9,16 +9,83 @@ from scipy.signal import find_peaks
 import matplotlib.dates as mdates
 from scipy.optimize import curve_fit
 import sys
+import os
+import re
+from tqdm.notebook import trange, tqdm
+from scipy.interpolate import interp1d
 #plt.style.use('~/evanstyle.mplstyle')
 
 
-class SRSReader:
-    def __init__(self, infile, points_per_amu=20, min_mass=1):
+class RGAReader:
+    def __init__(self, infile, rga_kind, points_per_amu=20, min_mass=1):
         self.infile = infile
         self.data = [] #each index is a new scan, with its elements being dict with timestamp and new values
         self.ppa = points_per_amu
         self.min_mass = min_mass 
         #maximum mass is assumed by how many points are in the scan and points_per_amu
+        self.rga_kind = rga_kind
+
+
+    def load_data(self):
+        if('mks' in self.rga_kind.lower()):
+            self.load_data_mks()
+        else:
+            self.load_data_srs()
+
+
+    #many scans stored in a single file. Many lines
+	#of a header. Then, there is a line that defines
+	#the "columns" that represent the scan time, and the
+	#masses of each measurement point. Every scan is on
+	#a single line. 
+    def load_data_mks(self):
+        if(os.path.isfile(self.infile) == False):
+            print("Could not find file : " + infile)
+            return
+
+        #load in the file reader
+        f = open(self.infile, 'r')
+        flines = f.readlines()
+        #header is always 0:57
+        #column line is line 56
+        columnline = flines[56] #has mass values
+
+        #splitting up all the garbage
+        columnline = columnline.split('"')
+        masses = []
+        for c in columnline:
+            if(len(c.split(' ')) == 2):
+                masses.append(float(c.split(' ')[-1]))
+        masses = sorted(masses)
+        
+        #compress the mass information into points per amu and min mass, to be
+        #consistent with the SRS reader. 
+        self.ppa = np.abs(masses[0] - masses[1])
+        self.min_mass = np.min(masses)
+
+        flines = flines[57:] #the rest are individual scans
+
+        datetime_format = "%m/%d/%Y %I:%M:%S %p"
+        looper = tqdm(flines)
+        counter = 0
+        for event in looper:
+            if(len(event) < 1000):
+                #likely at the end of file, some spaces or new lines
+                continue
+            t = event.split('"')[1] #the date timestamp of the scan
+            t = datetime.strptime(t, datetime_format)
+
+            #get the pressure values
+            p = re.findall(r"[^,\s]+", event)
+            #ignore the timestamp and such
+            p = p[4:-1] #last element is bad for some reason?
+            if(len(p) != len(masses)):
+                print("Something wierd happened with number of data points")
+                print(len(p))
+                print(len(masses))
+                continue
+
+            self.data.append({"time":t, "pressures": [float(_) for _ in p], "min_amu":np.min(masses), "max_amu": np.max(masses)})
 
 
 
@@ -27,7 +94,7 @@ class SRSReader:
     #in the binary (or I can't find them yet). SO i am compromizing
     #and assuming a min of 1 amu and 20 points per scan to infer the max AMU.
     #This is something to take note when setting up the continuous scanning. 
-    def load_data(self):
+    def load_data_srs(self):
         print("opening data file: " + self.infile)
         f = open(self.infile, 'rb')
         content = f.read()
@@ -62,6 +129,8 @@ class SRSReader:
         date_format = "%b %d, %Y  %I:%M:%S %p"
         for didx in date_indices:
             date_str = content[didx-1:didx+date_length].decode("ISO-8859-1")
+            if(didx == np.min(date_indices)):
+                print("First scan in this file is " + date_str)
             scan_dates.append(datetime.strptime(date_str, date_format))
 
 
@@ -71,12 +140,16 @@ class SRSReader:
         print("Grabbing pressures from each scan")
         min_amu = 1
         max_amu = None #to determine
+        pps = []
         for i in range(len(start_indices)):
             scan_idx = start_indices[i]
             if(i + 1 < len(start_indices)):
                 end_idx = date_indices[i+1] - 1 #the end of the scan (variable based on settings) is referenced to the date of next scan
             else:
-                break #not sure yet how to handle last scan, this throws it out. 
+                #break #not sure yet how to handle last scan, this throws it out. 
+                #or attempt to handle it by setting end_idx to be last packet in file
+                end_idx = len(content) #this does seem to work. 
+
 
             st = scan_idx
             db = 4
@@ -98,6 +171,9 @@ class SRSReader:
         #based on the number of data points in the pressures list. This allows
         #scans of different mass ranges to be in the same dataset, i.e. when chaining
         #datasets together.
+        if(len(pps) == 0):
+            print("Error! No data found in file " + self.infile)
+            return
         max_amu = int(len(pps)/self.ppa)
         for k in self.data:
             k["min_amu"] = self.min_mass
@@ -111,7 +187,7 @@ class SRSReader:
         #infile gets turned into a list, breaking functionality of
         #the "load_data" or "debug_binary" system - this just adds the self.data
         #lists and makes sure scans are in time order. 
-        s = SRSReader(infile=[self.infile, other.infile])
+        s = RGAReader(infile=[self.infile, other.infile], rga_kind=self.rga_kind)
         my_t0 = self.data[0]["time"]
         other_t0 = other.data[0]["time"]
         if(my_t0 > other_t0):
@@ -200,13 +276,16 @@ class SRSReader:
 
 
 
-    def plot_scan(self, scan_idx, ax=None):
+    def plot_scan(self, scan_idx, ax=None, label=None):
         if(ax == None):
             fig, ax = plt.subplots(figsize=(14, 8))
         e = self.data[scan_idx]
         masses = np.linspace(e["min_amu"], e["max_amu"], len(e["pressures"]))
         ax.set_title("Scan at: " + e["time"].strftime("%b %d, %Y  %H:%M:%S"))
-        ax.plot(masses, e["pressures"])
+        if(label == None):
+            ax.plot(masses, e["pressures"])
+        else:
+            ax.plot(masses, e["pressures"], label=label)
         ax.set_xlabel("M/Q (amu)")
         ax.set_ylabel("Torr")
         ax.set_yscale('log')
@@ -215,7 +294,7 @@ class SRSReader:
         ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
         ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
         ax.grid(True)
-        return fig, ax
+        return ax
 
 
 
@@ -223,7 +302,7 @@ class SRSReader:
     #window is a window of amu for the peak finding algorithm
     #fit is whether you want to fit a particular region (set by mintime and maxtime datetime objects)
     #duration is whether you want the primary x axis to be duration or datetimes
-    def plot_mass_evolution_peakfound(self, mass, ax=None, window=5, fit=False, mintime=None, maxtime=None, duration=True):
+    def plot_mass_evolution_peakfound(self, mass, ax=None, window=5, fit=False, mintime=None, maxtime=None, duration=True, dates=True, label=None):
         if(ax is None):
             fig, ax = plt.subplots(figsize=(14,8))
 
@@ -239,7 +318,7 @@ class SRSReader:
             ps.append(p)
             peakfound_masses.append(m)
 
-        if(duration):
+        if(duration and dates):
             #make an x axis on top with date labels
             axd = ax.twiny()
             l = axd.plot(ts, ps)
@@ -250,11 +329,14 @@ class SRSReader:
 
         mstd = np.std(peakfound_masses)
         massmean = np.mean(peakfound_masses)
+        if(label is None):
+            label = str(round(massmean, 2)) + " amu +- " + str(round(mstd, 2))
+
         if(duration):
-            ax.plot(ts_h, ps, '.',label=str(round(massmean, 2)) + " amu +- " + str(round(mstd, 2)))
+            ax.plot(ts_h, ps, '.',label=label)
             ax.set_xlabel("Hours since start")
         else:
-            ax.plot(ts, ps, '.',label=str(round(massmean, 2)) + " amu +- " + str(round(mstd, 2)))
+            ax.plot(ts, ps, '.',label=label)
             ax.xaxis.set_major_formatter(mdates.DateFormatter("%m/%d\n %H:%M"))
 
         ax.set_ylabel("Torr")
@@ -328,12 +410,13 @@ class SRSReader:
         #add an x axis on the top for dates
 
         return ax
+    
 
     #find the peak of the pressure at mass
     #for scan i in the dataset by (1) gaussian
     #smoothing and (2) peakfinding then (3) determinig
     #which peak is closest to mass
-    def get_peakfound_pressure(self, mass, i, window=5):
+    def get_peakfound_pressure(self, mass, i, window=2):
         #get pressures
         e = self.data[i]
         ms = np.linspace(e["min_amu"], e["max_amu"], len(e["pressures"]))
@@ -359,7 +442,7 @@ class SRSReader:
 
         #find all peaks. 
         #0.5 amu distance between peaks at least
-        peaks, _ = find_peaks(ps_sm, distance=0.5/dm, height=5e-10)
+        peaks, _ = find_peaks(ps_sm, distance=0.5/dm, height=5e-12)
         if(len(peaks) == 0):
             #no peaks found, just take value closest
             #closest value in masses list
@@ -401,10 +484,124 @@ class SRSReader:
         guess = [1e4, np.mean(ps), np.mean(ts)]
         popt, pcov = curve_fit(fitfunc, ts_s, ps, p0=guess)
 
+    #plot time information about scans contained in the file
+    def plot_scan_times(self, ax = None):
+        if(ax is None):
+            fig, ax = plt.subplots(figsize=(14,8))
+        
+        for e in self.data:
+            ax.scatter(e["time"], np.max(e["pressures"]))#, color='k', s=50)
+        
+        return ax
+
+    def get_time_of_scan(self, i):
+        e = self.data[i]
+        return(e["time"])
+    
+    #return the scan index closest to the input datetime object
+    def get_idx_at_time(self, t):
+        event = min(self.data, key=lambda x: abs((x["time"] - t).total_seconds()))
+        return self.data.index(event)
+
+    #t in hours
+    def get_idx_at_duration(self, t):
+        t0 = self.data[0]["time"] #assuming sorted. 
+        tf = t0 + timedelta(hours=t)
+        return self.get_idx_at_time(tf)
 
 
+    #get total pressure by integrating the spectrum
+    #for a particular scan index
+    def get_total_pressure(self, i):
+        e = self.data[i]
+        masses = np.linspace(e["min_amu"], e["max_amu"], len(e["pressures"]))
+        tot_p = np.trapz(e["pressures"], x=masses)
+        return tot_p
+    
+    def plot_total_pressure(self, ax=None, duration=True, dates=True, label=""):
+        if(ax is None):
+            fig, ax = plt.subplots(figsize=(14,8))
+
+        ps = []
+        ts = []
+        ts_h = [] #time in hours since start
+        for i, event in enumerate(self.data):
+            ts.append(event["time"])
+            ts_h.append((ts[-1] - ts[0]).total_seconds()/3600)
+            ps.append(self.get_total_pressure(i))
+
+        if(duration and dates):
+            #make an x axis on top with date labels
+            axd = ax.twiny()
+            l = axd.plot(ts, ps)
+            l.pop().remove() #remove the line from plot
+            axd.xaxis.set_major_formatter(mdates.DateFormatter("%m/%d\n %H:%M"))
+            axd.grid(False)
 
 
+        if(duration):
+            ax.plot(ts_h, ps, '.',label=label)
+            ax.set_xlabel("Hours since start")
+        else:
+            ax.plot(ts, ps, '.',label=label)
+            ax.xaxis.set_major_formatter(mdates.DateFormatter("%m/%d\n %H:%M"))
 
+        ax.set_ylabel("Torr")
+        ax.set_yscale('log')
+        return ax
+
+
+    #takes a single scan index and exports a file
+    #that is importable to the nist RGA database tool MS Search. 
+    #This file format is just a list of peak AMU's and their relative
+    #magnitudes, but would not necessarily take fractional AMUs. So,
+    #the user should list a set of relevant peaks, this function then 
+    #does peakfinding to fit the magnitude, and organizes into the correct file format. 
+    def export_for_nist(self, idx, peaks, outfile):
+        ps = []
+        good_peaks = []
+
+        #if you want to check every integer amu, pass empty list
+        if(peaks == []):
+            e = self.data[idx]
+            calib_factor = 0.5 #shift data up this many amu 
+            #gaussian smooth the data 
+            ms = np.linspace(e["min_amu"], e["max_amu"], len(e["pressures"]))
+            ms = np.array(ms) + calib_factor
+            ps_w = e["pressures"]  
+            ps_w = [(_ > 0)*_ for _ in ps_w]
+            ps_s = interp1d(ms, ps_w)
+            int_masses = np.array(list(set(np.array(ms).astype(int))))
+            good_peaks = int_masses
+            ps = ps_s(int_masses)
+            
+
+        else:  
+            for m in peaks:
+                p, m_ret = self.get_peakfound_pressure(m, idx, window=2)
+                if(np.abs(m - m_ret) >= 1):
+                    print("Couldn't get peakvalue for {:.1f} amu because of close proximity to {:.1f} amu peak".format(m, m_ret))
+                else:
+                    good_peaks.append(m)
+                    ps.append(p)
+
+        header = "NAME:" + datetime.strftime(self.data[idx]["time"], "%b %d, %Y  %H:%M:%S") + " idx " + str(idx) + "\n"
+        header += "COMMENT: \nFORMULA:\nMW:\nCAS:\nNum Peaks: {:d}\n".format(len(good_peaks))
+
+        #it seems the MS search program doesn't like small numbers.
+        #normalize such that the max peak is 1000
+        norm = np.max(ps)/1000
+
+        datastr = ""
+        for i in range(len(ps)):
+            if(int(ps[i]/norm) < 1):
+                continue
+            datastr += "{:d} {:d};\n".format(int(good_peaks[i]), int(ps[i]/norm))
+
+        outf = open(outfile, "w")
+        outf.write(header+datastr)
+        outf.close()
+
+        return good_peaks, ps
 
 
